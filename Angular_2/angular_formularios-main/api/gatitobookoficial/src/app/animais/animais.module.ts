18b3a8a6cebe6129b51ec6308a2d1804b466ac16
JulioTavaresDE <108236055+JulioTavaresDE@users.noreply.github.com>
2022-07-07 09:55:45 +0200

    sengundo

--- Angular_2/angular_formularios-main/api/gatitobookoficial/src/app/animais/animais.module.ts
+++ Angular_2/angular_formularios-main/api/gatitobookoficial/src/app/animais/animais.module.ts
@@ -1,7 +1,6 @@
 import { CartaoModule } from './../componentes/cartao/cartao.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
