2317777b1beb4c2a1692409b647033d4e1cf089c
JulioTavaresDE <108236055+JulioTavaresDE@users.noreply.github.com>
2022-07-09 14:10:47 +0200

    Dezimo

--- Angular_2/angular_formularios-main/api/gatitobookoficial/src/app/animais/animais-routing.module.ts
+++ Angular_2/angular_formularios-main/api/gatitobookoficial/src/app/animais/animais-routing.module.ts
@@ -1,25 +1,30 @@
-import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
-import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
-import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
+import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
+import { NovoAnimalComponent } from './novo-animal/novo-animal.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component:ListaAnimaisComponent,
+    path: '',
+    component: ListaAnimaisComponent,
     resolve: {
-      animais:ListaAnimaisResolver,
+      animais: ListaAnimaisResolver,
     },
   },
   {
-    path:':animalId',
-    component:DetalheAnimalComponent,
+    path: 'novo',
+    component: NovoAnimalComponent,
+  },
+  {
+    path: ':animalId',
+    component: DetalheAnimalComponent,
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AnimaisRoutingModule { }
+export class AnimaisRoutingModule {}
--- Angular_2/angular_formularios-main/api/gatitobookoficial/src/app/animais/animais.service.ts
+++ Angular_2/angular_formularios-main/api/gatitobookoficial/src/app/animais/animais.service.ts
@@ -1,47 +1,52 @@
-import { of } from 'rxjs';
-import { environment } from './../../environments/environment';
-import { Animais, Animal } from './animais';
-import { Observable, throwError } from 'rxjs';
-import { TokenService } from './../autenticacao/token.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
 import { catchError, mapTo } from 'rxjs/operators';
-import { ThrowStmt } from '@angular/compiler';
-
+import { environment } from 'src/environments/environment';
+import { TokenService } from '../autenticacao/token.service';
+import { Animais, Animal } from './animais';
 
 const API = environment.apiURL;
 const NOT_MODIFIED = '304';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnimaisService {
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
-  constructor(private http:HttpClient, private tokenService:TokenService) { }
-
-  listaDoUsuario(nomeDoUsuario:string):Observable<Animais>{
+  listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
     return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`);
   }
 
-
-  buscaPorID(id:number):Observable<Animal>{
-      return this.http.get<Animal>(`${API}/photos/${id}`);
-    }
-
-  excluiAnimal(id:number):Observable<Animal>{
-      return this.http.delete<Animal>(`${API}/photos/${id}`);
+  buscaPorID(id: number): Observable<Animal> {
+    return this.http.get<Animal>(`${API}/photos/${id}`);
   }
 
-  curtir(id:number): Observable<boolean>{
+  excluiAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id: number): Observable<boolean> {
     return this.http
-    .post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
-    .pipe(
+      .post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
+      .pipe(
         mapTo(true),
-        catchError((error)=> {
+        catchError((error) => {
           return error.status === NOT_MODIFIED ? of(false) : throwError(error);
-      })
+        })
       );
   }
+
+  upload(descricao: string, permiteComentario: boolean, arquivo: File) {
+    const formData = new FormData();
+    formData.append('description', descricao);
+    formData.append('allowComments', permiteComentario ? 'true' : 'false');
+    formData.append('imageFile', arquivo);
+
+    return this.http.post(`${API}/photos/upload`, formData, {
+      observe: 'events',
+      reportProgress: true,
+    });
+  }
 }
-
-
